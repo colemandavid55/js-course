c7d2410338a27da84389914a43a85e886285b34f
David Coleman <colemandavid55@gmail.com>
2014-12-26 19:54:39 -0600

    forgot to commit changes, this commit includes nearly all of console ui functionality

--- src/checkers.js
+++ src/checkers.js
@@ -13,4 +13,85 @@ var resetBoard = function () {
   ];
 
   currentPlayer = 'wht'
+  $(document).trigger('boardChange', board);
 };
+
+var attemptMove = function(row1, col1, row2, col2) {
+  if ( board[row1][col1] == 'wht' && currentPlayer == 'wht') {
+    //player selects to move traditionally, one diagonally space at a time
+    if ( (row2 == row1 + 1) && ((col2 == col1 - 1)||(col2 == col1 + 1)) ) {
+      //if they want to land on an untaken space
+      if ( board[row2][col2] == ' X ') {
+        makeMove(row1, col1, row2, col2);
+        return true;
+      }
+    } else {
+      $(document).trigger('invalidMove', "That move is invalid");
+      return false;
+    }
+    //player selects to jump, moving two spaces diagonally at a time
+    if ( (row2 == row1 + 2) && ((col2 == col1 - 2)||(col2 == col1 + 2)) ) {
+      //if they are planning to jump a piece of the opposite color
+      if ( board[row1+1][col1-1] == 'red') {
+        removePiece(row1+1, col1-1);
+        makeMove(row1, col1, row2, col2);
+        return true;
+      }
+      if ( board[row1+1][col1+1] == 'red') {
+        removePiece(row1+1, col1+1);
+        makeMove(row1, col1, row2, col2);
+        return true;
+      }
+    } else {
+      $(document).trigger('invalidMove', "That move is invalid")
+      return false;
+    }
+  } else if ( board[row1][col1] == 'red' && currentPlayer == 'red') {
+    if ( (row2 == row1 - 1) && ((col2 == col1 - 1)||(col2 == col1 + 1)) ){
+      //if they want to land on an untaken space
+      if ( board[row2][col2] == ' X ') {
+        makeMove(row1, col1, row2, col2);
+        return true;
+      }
+    }
+    //player selects to jump, moving two spaces diagonally at a time
+    if ( (row2 == row1 - 2) && ((col2 == col1 - 2)||(col2 == col1 + 2)) ) {
+      //if they are planning to jump a piece of the opposite color
+      if ( board[row1-1][col1-1] == 'wht') {
+        removePiece(row1-1, col1-1);
+        makeMove(row1, col1, row2, col2);
+        return true;
+      }
+      if ( board[row1-1][col1+1] == 'wht') {
+        removePiece(row1-1, col1+1);
+        makeMove(row1, col1, row2, col2);
+        return true;
+      }
+    }
+  } else {
+    $(document).trigger('invalidMove', "That move is invalid");
+    return false;
+
+  }
+  
+};
+
+var makeMove = function(row1, col1, row2, col2) {
+  board[row2][col2] = board[row1][col1];
+  board[row1][col1] = ' X ';
+  if (currentPlayer == "wht") {
+    currentPlayer = 'red';
+  } else {
+    currentPlayer = 'wht';
+  }
+  $(document).trigger('boardChange', board);
+  $(document).trigger('moveMade', moveCount);
+};
+
+var removePiece = function(row, col) {
+  board[row][col] = ' X ';
+  $(document).trigger('boardChange', board);
+  $(document).trigger('pieceTaken', taunt);
+};
+
+
--- src/console_driver.js
+++ src/console_driver.js
@@ -10,6 +10,30 @@ var charToNum = {
   h: 7
 }
 
+var displayError = function(e, error) {
+  console.log(error);
+};
+
+var count = 0;
+
+var moveCount = function () {
+  count += 1;
+  console.log(count)
+
+}
+
+var play = function () {
+  resetBoard();
+
+  while (true) {
+    var move = getMove();
+    if (move.quit) {
+      break;
+    }
+    attemptMove(move.startRow, move.startCol, move.endRow, move.endCol);
+  }
+}
+
 var displayBoard = function () {
   var column = [0, 1, 2, 3, 4, 5, 6, 7];
   console.log("  | " + column.join("   "));
@@ -18,3 +42,34 @@ var displayBoard = function () {
     console.log(numToChar[i] + " |" + board[i].join(" "));
   }
 };
+
+var getMove = function() {
+  var start = prompt("Current Player: " + currentPlayer + "\nPick starting piece position (example: f5)");
+  if (start[0] == "q") {
+    return {
+      quit: true
+    }
+  }
+  var end = prompt("Pick ending position");
+
+  start = start.split("")
+  end = end.split("")
+
+  var moveObject = {
+    startRow: charToNum[start[0]],
+    startCol: Number(start[1]),
+    endRow: charToNum[end[0]],
+    endCol: Number(end[1])
+  };
+  return moveObject;
+};
+
+var taunt = function () {
+  console.log("You want to keep your pieces, not give them away...");
+}
+
+$(document).on('invalidMove', displayError);
+$(document).on('boardChange', displayBoard);
+$(document).on('pieceTaken', taunt);
+$(document).on('moveMade', moveCount);
+
