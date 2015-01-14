ac64a642f10e2366c98827d1785e444d6002708e
David Coleman <colemandavid55@gmail.com>
2015-01-14 10:24:47 -0600

    capture changes before code overhaul

--- src/task-presenter.js
+++ src/task-presenter.js
@@ -2,10 +2,44 @@
 
 (function () {
 
+  window.Quizpresenter = {}
+
   // TODO: REMOVE
-  Task.add('my task')
-  Task.add('another one')
-  Task.add('do itt')
+  Quiz.addQuestion("Which bear is best?",
+    [
+    "Black Bear",
+    "Bears are all terrible",
+    "Cats and Dogs FTW"
+    ]
+  )
+  Quiz.addQuestion("Why is the meaning of the universe?",
+    [
+    "Therefore",
+    "Nevermore",
+    "Futhermore"
+    ]
+  )
+  Quiz.addQuestion("What is your favorite thing?",
+    [
+    "People",
+    "Events",
+    "Ideas"
+    ]
+  )
+  Quiz.addQuestion("Why do good things happen to bad people?",
+    [
+    "Define 'good'",
+    "Define 'bad'",
+    "Dude, whatever."
+    ]
+  )
+  Quiz.addQuestion("How do you feel about this quiz?",
+    [
+    "Good",
+    "Bad",
+    "There is no measurable level of interest"
+    ]
+  )
   // ^^^^^^^^^^^^^^
 
   // Task.forEach(function (task) {
@@ -17,34 +51,112 @@
   //   $('#task-list').append(li)
 
   // })
+   // var answersKey = {
+   //              100: "Bears are all terrible",
+   //              101: "Nevermore",
+   //              102: "Ideas",
+   //              103: "Dude, whatever.",
+   //              104: "Bad"
+   //              };
 
-  renderTasks()
 
-  $(document).on('new-task', renderTasks)
+  renderQuiz()
 
-    function renderTasks () {
-      $('#task-list').empty()
-      Task.forEach(function (task) {
-        var li = $('<li>').append(
-          $('<input type="checkbox">')
-            .attr('data-id', task.id)
-            .prop("checked", task.completed),
-          $('<span>').addClass('name').text(task.name)
-        )
+  var userAnswers = {};
 
-        $('#task-list').append(li)
 
+  function renderQuiz () {
+    userAnswers = {};
+    $('#quiz').empty()
+    var questionForms = Quiz.questions.map(function (question) {
+      console.log(question)
+      console.log(question["answers"])
+      var answer = this.value
+      var answerMenu = $('<select id="' + question.id + '">')
+      answerMenu.on('change', function (e) {
+        userAnswers[question.id] = answer
+        console.log(userAnswers)
       })
-    }
+      return $('<form>').append(
+        $('<label id="question">')
+        .attr('data-id', question.id)
+        .text(question.content)
+        .append(answerMenu),
+        answerMenu.append(question.answers.map(function (answer) {
+          return $('<option id="answer">')
+            .attr('data-id', answer.id)
+            .text(answer)
+
+          }) 
+        )
+      )
+    })
+    $('#quiz').append(questionForms)
+
+
+      
+    var button = $('<button type="submit">').text("Submit For Judgement!")
+    var nameHere = $('<input type="text" id="name">')
+    var label = $('<label type="text">').text("Type Name Here")
+    label.append(nameHere)
+    $('#quiz').append(label, '<br>', '<br>', button)
+  }
 
     //List to the view
-    $('#task-list').on('click', 'input[type=checkbox]', function (e) {
-      // ON click, update model
-      var taskId = $(this).attr('data-id');
-      var isChecked = $(this).prop('checked');
-      console.log("Checked?", isChecked, taskId)
+    // $('#task-list').on('click', 'input[type=checkbox]', function (e) {
+    //   // ON click, update model
+    //   var taskId = $(this).attr('data-id');
+    //   var isChecked = $(this).prop('checked');
+    //   console.log("Checked?", isChecked, taskId)
 
-      Task.markComplete(parseInt(taskId, 10), isChecked)
+    //   Task.markComplete(parseInt(taskId, 10), isChecked)
+    // })
+
+  $(document).on('click', 'button', function(e) {
+    e.preventDefault();
+    // var answersSubmitted = {
+    //   id: $('')
+    // }
+    //Register answers to questions
+    var name = $('#name').val()
+
+
+    var score = Quiz.gradeQuiz(userAnswers);
+    Quiz.logResult(name, score)
+    $('form#quiz').empty()
+    $('.score').empty()
+    $('.leaders').empty()
+
+    // console.log("click received")
+    // for (var qId in userAnswers) {
+    //   if (userAnswers[qId] === answersKey[qId]) {
+    //     score++
+    //   }
+    // }
+
+    var div = $('<div>').addClass("score").append('<h3>').text("Your score on the quiz: " + score)
+    $('body').append(div)
+
+    var leaders = Quiz.highScores()
+    var leaderDiv = $('.leaders')
+    var sortedLeaders = leaders.sort(function (leaderA, leaderB) {
+      return leaderB["score"] - leaderA["score"];
     })
+    var $sortedLeaders = sortedLeaders.map(function (leader) {
+      return $('<p>').text("Name: " + leader["name"] + ": " + leader["score"])
+    })
+    // var unsortedLeaders = leaders.map(function (leader) {
+    //   console.log(leader);
+    //   var leaderName = leader["name"]
+      
+    // })
+    // console.log(unsortedLeaders)
+    // var sortedLeaders = unsortedLeaders.sort(function (a, b) {
+    //   return b["score"] - a["score"];
+    // })
+    leaderDiv.append($sortedLeaders);
+    renderQuiz();
+  })
+
 
 })()
--- src/tasks.js
+++ src/tasks.js
@@ -1,28 +1,60 @@
 (function () {
 	//Code!
   //Private
-  var tasks = []
+  // Quiz.questions = []
   var idCounter = 100
+  var answersKey = {
+                100: "Bears are all terrible",
+                101: "Nevermore",
+                102: "Ideas",
+                103: "Dude, whatever.",
+                104: "Bad"
+                };
 
   //Public
-  window.Task = {}
+  window.Quiz = {}
+
+  Quiz.questions = []
+  Quiz.leaderBoard = []
+  Quiz.results = []
+  Quiz.answerFrequency ={}
+  //  { 100: {
+
+  // }}
+
+
+ // "Black Bear",
+ //    "Bears are all terrible",
+ //    "Cats and Dogs FTW"
+
+  Quiz.gradeQuiz = function (userAnswers) {
+    // Maniuplate score
+    // Return true or false
+    var score = 0;
+    for (var answer in userAnswers) {
+      if (answer[question.id] === answersKey[answer[question.id]]) {
+        score++
+      }
+    }
+    console.log(score);
+    idCounter = 100;
+    return score;
+  }
 
   // Transaction Script
-  Task.add = function (name) {
+  Quiz.addQuestion = function (content, answers) {
     // TODO: Add validation (ensure name is at least X characters, ...)
-    if (! name) {
-      return { error: 'name_required' }
+    if (! content) {
+      return { error: 'content_required' }
     }
-    if ( name.length < 3) {
-      return { error: 'name_too_short' }
-    }
-    var task = {
+    var question = {
       id: idCounter,
-      name: name,
+      content: content,
+      answers: answers,
       completed: false
     }
 
-    tasks.push(task)
+    Quiz.questions.push(question)
     idCounter += 1
 
     // Publish
@@ -30,36 +62,61 @@
 
     //Publish Event
     //Open-Closed principle (solid)
-    $(document).trigger('new-task')
+    // $(document).trigger('new-task')
 
     return { success: true }
   }
 
-  Task.find = function (id) {
-    for (var i=0; i < tasks.length; i++) {
-      if (tasks[i].id === id) {
-        return tasks[i]
+  // Task.find = function (id) {
+  //   for (var i=0; i < tasks.length; i++) {
+  //     if (tasks[i].id === id) {
+  //       return tasks[i]
+  //     }
+  //   }
+  // }
+
+  // Task.markComplete = function (taskId, isComplete) {
+  //   var task = Task.find(taskId)
+  //   if (task) {
+  //     task.completed = !!isComplete
+  //   }
+  // }
+
+  Quiz.forEach = function (callback) {
+    for (var i = 0; i < questions.length; i++ ) {
+      callback( extend({}, questions[i]) )
+    }
+  }
+
+  Quiz.logResult = function (name, score) {
+    Quiz.results.push({"name": name, "score": score})
+    return true
+  }
+
+  Quiz.highScores = function() {
+    return Quiz.results.filter(function (entry) {
+      if ( entry["score"] >= 3 ) {
+        return entry;
       }
-    }
+    })
+    // if ( ! Quiz.leaderBoard[name] ) {
+    //   Quiz.leaderBoard[name] = score;
+    // } else {
+    //   Quiz.leaderBoard[name] = (Quiz.leaderBoard[name] >= score) ? Quiz.leaderBoard[name] : score;
+    // }
+    // return Quiz.leaderBoard
   }
 
-  Task.markComplete = function (taskId, isComplete) {
-    var task = Task.find(taskId)
-    if (task) {
-      task.completed = !!isComplete
-    }
-  }
+  // Quiz.countAnswers = function(userAnswers) {
+  //   if (Quiz.answerFrequency[question.id] === userAnswers[question.id] ) {
+  //     Quiz.answerFrequency[question.id] = QuizanswerFrequency[question.id] || 1
+  //   }
+  // }
 
-  Task.forEach = function (callback) {
-    for (var i = 0; i < tasks.length; i++ ) {
-      callback( extend({}, tasks[i]) )
-    }
-  }
-
-  //Meanwhile...
-  Task.forEach(function (task) {
-    console.log("I have a task: ", task.name)
-    $('<div class="task">').text(task.name)
-  })
+  // //Meanwhile...
+  // Task.forEach(function (task) {
+  //   console.log("I have a task: ", task.name)
+  //   $('<div class="task">').text(task.name)
+  // })
 
 })()
